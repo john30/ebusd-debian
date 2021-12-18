300078d542217f147e6d81eac3c5ae7e91b8e5de
John <ebusd@ebusd.eu>
2021-12-18 12:13:07 +0100

    formatting

--- sign.sh
+++ sign.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 gpg --no-use-agent --digest-algo SHA256 -u ebusd@ebusd.eu --passphrase "$PASS" --clearsign -o "$2" "$1"
-gpg --no-use-agent --digest-algo SHA256 -u ebusd@ebusd.eu  --passphrase "$PASS" -abs -o "$3" "$1"
+gpg --no-use-agent --digest-algo SHA256 -u ebusd@ebusd.eu --passphrase "$PASS" -abs -o "$3" "$1"
 
